a59a6bd356f1823e9746947faddd0521d9d60600
Jon Cockayne <jcockayne@turing.ac.uk>
2021-08-02 10:53:55 +0100

    Update to new version of API

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy
+from distutils.extension import Extension
 
 setup(
     name='bayesian_pdes',
@@ -12,6 +13,10 @@ setup(
     author_email='',
     description='',
     requires=['numpy'],
-    ext_modules=cythonize('bayesian_pdes/pairwise.pyx'),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize([
+        Extension('*',
+            ['bayesian_pdes/pairwise.pyx'],
+            include_dirs=[numpy.get_include()]
+        )
+    ])
 )
